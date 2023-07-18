4a220482bf663f5fc33715f680cb2f83057ee213
amd <amd@devin.com>
2023-07-18 23:21:05 +0900

    컴포넌트 스캔과 자동 의존관계 설정
    스프링 빈과 의존관계
    
    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술

--- src/main/java/hello/hellospring3/service/MemberService.java
+++ src/main/java/hello/hellospring3/service/MemberService.java
@@ -2,15 +2,18 @@ package hello.hellospring3.service;
 
 import hello.hellospring3.domain.Member;
 import hello.hellospring3.repository.MemberRepository;
-import hello.hellospring3.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }

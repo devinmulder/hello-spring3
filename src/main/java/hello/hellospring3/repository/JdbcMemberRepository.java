8332afc1db8435f6cec50420972c972eeb1d4324
amd <amd@devin.com>
2023-07-19 09:58:31 +0900

    누락 코드 반영.

--- src/main/java/hello/hellospring3/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring3/repository/JdbcMemberRepository.java
@@ -75,7 +75,30 @@ public class JdbcMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+
+        String sql = "select * from member where name = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }
+            return Optional.empty();
+
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+
     }
 
     @Override

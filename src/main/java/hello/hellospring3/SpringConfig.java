3f08a6cd688f740209877350019f6c464f157106
amd <amd@devin.com>
2023-07-19 23:59:29 +0900

    JPA
    스프링 DB 접근 기술
    
    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술

--- src/main/java/hello/hellospring3/SpringConfig.java
+++ src/main/java/hello/hellospring3/SpringConfig.java
@@ -1,24 +1,21 @@
 package hello.hellospring3;
 
-import hello.hellospring3.repository.JdbcMemberRepository;
-import hello.hellospring3.repository.JdbcTemplateMemberRepository;
+import hello.hellospring3.repository.JpaMemberRepository;
 import hello.hellospring3.repository.MemberRepository;
-import hello.hellospring3.repository.MemoryMemberRepository;
 import hello.hellospring3.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -30,6 +27,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }

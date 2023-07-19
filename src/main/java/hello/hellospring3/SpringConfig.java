1ff94e169cfdf3d0f788da36bbcd024c15272ace
amd <amd@devin.com>
2023-07-19 20:42:01 +0900

    스프링 JdbcTemplate
    스프링 DB 접근 기술
    
    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술

--- src/main/java/hello/hellospring3/SpringConfig.java
+++ src/main/java/hello/hellospring3/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring3;
 
 import hello.hellospring3.repository.JdbcMemberRepository;
+import hello.hellospring3.repository.JdbcTemplateMemberRepository;
 import hello.hellospring3.repository.MemberRepository;
 import hello.hellospring3.repository.MemoryMemberRepository;
 import hello.hellospring3.service.MemberService;
@@ -28,6 +29,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }

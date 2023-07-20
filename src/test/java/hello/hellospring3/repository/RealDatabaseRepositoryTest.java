55ee65d603efb292cb86e5fa79eedac06002d119
amd <amd@devin.com>
2023-07-20 12:10:09 +0900

    실제 Database 를 대상으로 한 repository test 작성. test 성공.
    .
    
    * test 코드 작성하다가 부딪힌 문제
    
    Application 의 bean 설정의 영향 없이, [test 에서 별도로 bean 설정] 하는 방법이 궁금하다.
    
    test 에 @SpringBootTest 를 붙이니까, Application 의 bean 설정이 그대로 적용되어서
    test 에서 따로 bean 설정을 할 수가 없다.
    
    어떻게 해야 하나?
    김영한님 강의 중, test 에서 bean 설정하는 부분을 유심하게 보자.
    
    .
    
    * 의문점
    
    Repository 를 test 할 때는 DB 에 commit 하지 않도록 하기 위해 @Transactional 을 붙인다.
    
    @Transactional 은 스프링을 돌려야 하는 것이다.
    
    그러면, Repository test 코드는 항상 스프링으로 돌리나?
    
    Repository test 코드에는 항상 @SpringBootTest 를 붙이나?

---
+++ src/test/java/hello/hellospring3/repository/RealDatabaseRepositoryTest.java
@@ -0,0 +1,63 @@
+package hello.hellospring3.repository;
+
+import hello.hellospring3.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+class RealDatabaseRepositoryTest {
+
+    @Autowired
+    MemberRepository repository;
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+        //when
+        repository.save(member);
+        //then
+        Member result = repository.findById(member.getId()).get();
+        assertThat(result).isEqualTo(member);
+    }
+
+
+    @Test
+    void findByName() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+        //when
+        Member result = repository.findByName("spring1").get();
+        //then
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+        //when
+        List<Member> result = repository.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+}
